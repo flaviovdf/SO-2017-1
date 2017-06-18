d2f4dc60d7fc0a3d675477e3f81e5ba71e54b819
Flavio Figueiredo <flaviovdf@gmail.com>
2017-06-18 17:54:31 -0300

    Novo esqueleto

--- tp2/esqueletos/ahloka/aloca.h
+++ tp2/esqueletos/ahloka/aloca.h
@@ -1,17 +1,21 @@
 #ifndef SO605_GC
 #define SO605_GC
 
-#define MEMSIZE 4096*1024*1024            //Processo tem 4096 MB de memória
+// 4096*1024
+// Processo tem 4096 MB de memória
+#define MEMSIZE 4194304
 
 //Nó da lista de memória livre
 typedef struct free_node {
+ size_t free;
  size_t size;
  struct free_node *next;
 } free_node_t;
 
-//Lista de memória alocada. Ponteira para
-//o início da lista.
-typedef *free_node_t mem_free_t;
+typedef struct {
+  free_node_t *head;
+  free_node_t *lastAlloca; // Usado para next fit
+} free_list_t;
 
 void *aloca(size_t size);
 void libera(void *ptr);
